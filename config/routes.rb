d3844058c98750818c937b20038c41840bc1051d
Stephanie <stephdacre@gmail.com>
2017-02-20 22:39:07 +0800

    generate clearance views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
+  resources :users, except: :index
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
