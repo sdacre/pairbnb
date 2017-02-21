c42573f0ae75b93a232fd7bb0c0498b4750a45f9
Stephanie <stephdacre@gmail.com>
2017-02-21 14:53:10 +0800

    use ajax for user authentication errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create
+	@user = User.new(user_params)
+	 
+		respond_to do |format|
+		  if @user.save
+		  format.html { redirect_to root_path, notice: 'User was successfully created.' }
+		  else
+		  format.js {  }
+		  end
+		end
+	end
+
+	private
+	def user_params
+	params.require(:user).permit(:email, :password)
+	end 
+
 end
