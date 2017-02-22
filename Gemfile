1f05de1d12d265445ae31c06ab3dbc095776066b
Stephanie <stephdacre@gmail.com>
2017-02-22 18:34:11 +0800

    create seed methods with faker

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+gem 'faker'
+#gem to create fake seed data
 gem 'omniauth-facebook'
 gem 'omniauth'
 #add facebook auth gems
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,44 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Tag.create(name: 'Smoking Allowed')
-Tag.create(name: 'Pets Allowed')
-Tag.create(name: 'Pool')
-Tag.create(name: 'Free Parking')
-Tag.create(name: 'Air Conditioning')
-Tag.create(name: 'Wifi')
-Tag.create(name: 'Heating')
+# Tag.create(name: 'Smoking Allowed')
+# Tag.create(name: 'Pets Allowed')
+# Tag.create(name: 'Pool')
+# Tag.create(name: 'Free Parking')
+# Tag.create(name: 'Air Conditioning')
+# Tag.create(name: 'Wifi')
+# Tag.create(name: 'Heating')
+
+# Seed Users
+user = {}
+user['password'] = 'asdf'
+
+ActiveRecord::Base.transaction do
+  20.times do 
+    user['first_name'] = Faker::Name.first_name 
+    user['last_name'] = Faker::Name.last_name
+    user['email'] = Faker::Internet.email
+
+    User.create(user)
+  end
+end 
+
+# Seed Listings
+listing = {}
+uids = []
+User.all.each { |u| uids << u.id }
+ActiveRecord::Base.transaction do
+  40.times do 
+    listing['home_type'] = ["Shared Room", "Private Room", "Entire Home/Apt"].sample
+    listing['listing_type'] = ["House", "Apartment"].sample
+    listing['accommodate'] = rand(0..16)
+    listing['bedroom'] = rand(1..10)
+    listing['bathroom'] = rand(1..8)
+    listing['address'] = Faker::Address.street_address
+    listing['summary'] = Faker::Hipster.sentence
+
+    listing['user_id'] = uids.sample
+
+    Listing.create(listing)
+  end
+end
